1bee7b7a26a041d50530afd2c0de3c4f59d9b8cb
skywalker <skywalker_x@163.com>
2018-08-31 16:20:23 +0800

    1. 配置热加载，可以按传入的命令行参数来加载不同模块

---
+++ server.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const argv = require('minimist')(process.argv.slice(2));
+
+const moduleName = argv["name"];
+const port = 10023;
+const config = require('./webpack.config.js');
+const options = {
+    contentBase: './dist',
+    hot: true,
+    host: 'localhost'
+};
+config.entry[moduleName] = path.resolve(__dirname, `package/${moduleName}/index.js`);
+config.mode = 'development';
+// 配置html-webpack-plugin
+config.plugins.push(
+    new HtmlWebpackPlugin({
+        template: `templates/${moduleName}/index.html`
+    })
+);
+
+WebpackDevServer.addDevServerEntrypoints(config, options);
+
+const compiler = webpack(config);
+const server = new WebpackDevServer(compiler, options);
+
+server.listen(port, 'localhost', ()=> {
+    console.log(`dev server is running at port ${port}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
